ee71edf87159e09df57163b1fd887ad3f1130605
Kevin-Li-Webjet <kevin.li@webjet.com.au>
2020-08-25 23:35:05 +1000

    Update test.sh

--- test.sh
+++ test.sh
@@ -48,7 +48,7 @@ main(){
     addBuildArgs
 
     # ImageName & Tag
-    BUILD_TAGS=" -t ${INPUT_IMAGENAME}:${INPUT_TAG} "
+    BUILD_TAGS=" . -t ${INPUT_IMAGENAME}:${INPUT_TAG} "
 
     echo "[INFO] BUILD COMMAND: docker build ${BUILDPARAMS} ${BUILD_TAGS}"
     docker build ${BUILDPARAMS} ${BUILD_TAGS}
