354f7d3891ff31ad31b0c7be7613b5311b4db78c
Kevin-Li-Webjet <kevin.li@webjet.com.au>
2020-08-25 23:24:10 +1000

    wip

--- test.sh
+++ test.sh
@@ -1,2 +1,57 @@
-# docker build --build-arg REGISTRY=$1 -t "$1/testimg:1" .
-# docker push $1/testimg:1
+#!/bin/sh
+
+
+## INPUTS
+INPUT_REGISTRY=$1
+INPUT_BUILDARGS=$2
+INPUT_IMAGENAME=$3
+INPUT_TAG=$4
+INOUT_DOCKERFILED_PATH=$5
+
+## FUNCTION
+addBuildArgs() {
+  for ARG in $(echo "${INPUT_BUILDARGS}" | tr ',' '\n'); do
+    BUILDPARAMS="${BUILDPARAMS} --build-arg ${ARG}"
+    echo "[INFO] add-arg::${ARG}"
+  done
+}
+
+sanitize() {
+  if [ -z "${1}" ]; then
+    >&2 echo "Unable to find the ${2}. Did you set with.${2}?"
+    exit 1
+  fi
+}
+
+useCustomDockerfile() {
+  echo "[INFO] add-docker-path::${INOUT_DOCKERFILED_PATH}"
+  BUILDPARAMS="${BUILDPARAMS} -f ${INOUT_DOCKERFILED_PATH}"
+}
+
+uses() {
+  [ ! -z "${1}" ]
+}
+
+
+#### MAIN
+main(){
+    BUILDPARAMS="--build-arg REGISTRY=${INPUT_REGISTRY}"
+    sanitize "${INPUT_IMAGENAME}" "Image Name"
+
+    # Check PATH
+    if uses "${INOUT_DOCKERFILED_PATH}"; then
+        useCustomDockerfile
+    fi
+
+    # Add Args
+    addBuildArgs
+
+    # ImageName & Tag
+    BUILD_TAGS=" -t ${INPUT_IMAGENAME}:${INPUT_TAG} "
+
+    echo "[INFO] BUILD COMMAND: docker build ${BUILDPARAMS} ${BUILD_TAGS}"
+    docker build ${BUILDPARAMS} ${BUILD_TAGS}
+}
+
+
+main
