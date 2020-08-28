125b03b85eb14922fa53b4e38a46bd230d180046
Kevin-Li-Webjet <kevin.li@webjet.com.au>
2020-08-28 16:38:18 +1000

    wip

--- download.sh
+++ download.sh
@@ -19,7 +19,7 @@ main(){
 
     sanitize "${INPUT_GITHUB_TOKEN}" "Github-access-token"
 
-    curl -v -L -u octocat:${INPUT_GITHUB_TOKEN} -o logs.zip "https://api.github.com/repos/${INPUT_COMPANY}/${INPUT_REPO}/actions/runs/${INPUT_RUN_NUM}/logs"
+    curl -v -L -u octocat:${INPUT_GITHUB_TOKEN} -o logs.zip "https://api.github.com/repos/${INPUT_COMPANY}/${INPUT_REPO}/actions/runs/${INPUT_RUNID}/logs"
 
 }
 
