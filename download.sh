74bf61a94296563cf6570a16c789d7a858113d81
Kevin-Li-Webjet <61395724+Kevin-Li-Webjet@users.noreply.github.com>
2020-08-30 21:17:34 +1000

    Update download.sh

--- download.sh
+++ download.sh
@@ -21,11 +21,11 @@ main(){
     
     ## OPTION1: WHOLE RUN LOG
     ## ! Can't download run log while build not finished
-   # curl -v -L -u octocat:${INPUT_GITHUB_TOKEN} -o logs.zip "https://api.github.com/repos/${INPUT_COMPANY}/${INPUT_REPO}/actions/runs/${INPUT_RUNID}/logs"
-    curl -v -L -u octocat:${INPUT_GITHUB_TOKEN} -o logs.zip "https://api.github.com/repos/Kevin-Li-Webjet/Git-Action-Test/actions/runs/228240146/logs"
+   # curl -v -L -u octocat:${INPUT_GITHUB_TOKEN} -o ${INPUT_FILENAME} "https://api.github.com/repos/${INPUT_COMPANY}/${INPUT_REPO}/actions/runs/${INPUT_RUNID}/logs"
+    # curl -v -L -u octocat:${INPUT_GITHUB_TOKEN} -o ${INPUT_FILENAME} "https://api.github.com/repos/Kevin-Li-Webjet/Git-Action-Test/actions/runs/228240146/logs" # TEST OK
     
     ## OPTION2: JOB LOG
-    curl -v -L -u octocat:${INPUT_GITHUB_TOKEN} -o logs.txt "https://api.github.com/repos/${INPUT_COMPANY}/${INPUT_REPO}/actions/jobs/${INPUT_JOBID}/logs"
+    curl -v -L -u octocat:${INPUT_GITHUB_TOKEN} -o ${INPUT_FILENAME} "https://api.github.com/repos/${INPUT_COMPANY}/${INPUT_REPO}/actions/jobs/${INPUT_JOBID}/logs"
 
 }
 
