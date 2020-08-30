37773476b9a7c36a7e6e76417e54385f27f75646
Kevin-Li-Webjet <61395724+Kevin-Li-Webjet@users.noreply.github.com>
2020-08-30 21:04:45 +1000

    Update download.sh

--- download.sh
+++ download.sh
@@ -18,7 +18,7 @@ sanitize() {
 main(){
 
     sanitize "${INPUT_GITHUB_TOKEN}" "Github-access-token"
-
+    ## ! Can't download run log while build not finished
    # curl -v -L -u octocat:${INPUT_GITHUB_TOKEN} -o logs.zip "https://api.github.com/repos/${INPUT_COMPANY}/${INPUT_REPO}/actions/runs/${INPUT_RUNID}/logs"
     curl -v -L -u octocat:${INPUT_GITHUB_TOKEN} -o logs.zip "https://api.github.com/repos/Kevin-Li-Webjet/Git-Action-Test/actions/runs/228240146/logs"
 
